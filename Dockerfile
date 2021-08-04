8f163e448300650d9cd7e85c043ed9de41a56ff2
Major Hayden <major@mhtx.net>
2021-08-04 10:59:27 -0500

    Initial import

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM registry.fedoraproject.org/fedora:rawhide
+RUN echo "fastestmirror=1" >> /etc/dnf/dnf.conf
+# RUN mkdir -vp /etc/selinux && touch /etc/selinux/config
+RUN dnf -y upgrade && \
+    dnf -y install composer-cli jq osbuild-composer uuid && \
+    dnf clean all
+RUN systemctl enable osbuild-composer.socket
+CMD ["/sbin/init"]
---
+++ build-image.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -euo pipefail
+
+BLUEPRINT_NAME=fedora
+SHIP_TO_AWS=yes
+
+docker-exec () {
+    docker exec -t imagebuilder $@
+}
+
+composer-cli () {
+    docker-exec composer-cli $@
+}
+
+# Start the docker container.
+docker run --detach --rm --privileged \
+    -v $(pwd)/shared:/repo \
+    --name imagebuilder \
+    imagebuilder
+
+# Wait for composer to be fully running.
+for i in `seq 1 10`; do
+    sleep 1
+    composer-cli status show && break
+done
+
+# Push the blueprint and depsolve.
+composer-cli blueprints push /repo/${BLUEPRINT_NAME}.toml
+composer-cli blueprints depsolve ${BLUEPRINT_NAME} > /dev/null
+composer-cli blueprints list
+
+IMAGE_UUID=$(docker-exec uuid)
+
+# Start the build.
+if [[ $SHIP_TO_AWS == "yes" ]]; then
+    composer-cli --json compose start ${BLUEPRINT_NAME} ami github-actions-${IMAGE_UUID} /repo/aws-config.toml | tee compose_start.json
+else
+    composer-cli --json compose start ${BLUEPRINT_NAME} ami | tee compose_start.json
+fi
+
+COMPOSE_ID=$(jq -r '.build_id' compose_start.json)
+
+# Watch the logs while the build runs.
+docker-exec journalctl -af &
+
+while true; do
+    composer-cli --json compose info "${COMPOSE_ID}" | tee compose_info.json > /dev/null
+    COMPOSE_STATUS=$(jq -r '.queue_status' compose_info.json)
+
+    # Is the compose finished?
+    if [[ $COMPOSE_STATUS != RUNNING ]] && [[ $COMPOSE_STATUS != WAITING ]]; then
+        echo "Compose finished."
+        break
+    fi
+    sleep 5
+done
+
+if [[ $COMPOSE_STATUS != FINISHED ]]; then
+    composer-cli compose logs ${COMPOSE_ID}
+    docker-exec tar -axf /${COMPOSE_ID}-logs.tar logs/osbuild.log -O
+    echo "Something went wrong with the compose. 😢"
+    exit 1
+fi
