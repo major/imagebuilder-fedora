e762a98971ee42d3e91ed1c046ce0fce70ba1f10
Major Hayden <major@mhtx.net>
2021-08-04 15:47:36 -0500

    Less output

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 BLUEPRINT_NAME=fedora
 CONTAINER_NAME=imagebuilder
@@ -18,30 +18,33 @@ composer-cli () {
 }
 
 # Start the container.
-# podman run --detach --rm --privileged \
-#     -v $(pwd)/shared:/repo \
-#     --name $CONTAINER_NAME \
-#     $DOCKER_IMAGE
+echo "🚀 Launching the container"
 sudo podman run --rm --detach --privileged \
     -v $(pwd)/shared:/repo \
     --name $CONTAINER_NAME \
     $DOCKER_IMAGE
 
 # Wait for composer to be fully running.
+echo "⏱ Waiting for composer to start"
 for i in `seq 1 10`; do
     sleep 1
     composer-cli status show && break
 done
 
-# Push the blueprint and depsolve.
+echo "📥 Pushing the blueprint"
 composer-cli blueprints push /repo/${BLUEPRINT_NAME}.toml
+
+echo "🔎 Solving dependencies in the blueprint"
 composer-cli blueprints depsolve ${BLUEPRINT_NAME} > /dev/null
+
+echo "🗒 Verifying that the blueprint exists"
 composer-cli blueprints list
 
-# Start the build.
 if [[ $SHIP_TO_AWS == "yes" ]]; then
+    echo "🛠 Build the image and ship to AWS"
     composer-cli --json compose start ${BLUEPRINT_NAME} ami github-actions-${IMAGE_UUID} /repo/aws-config.toml | tee compose_start.json
 else
+    echo "🛠 Build the image"
     composer-cli --json compose start ${BLUEPRINT_NAME} ami | tee compose_start.json
 fi
 
@@ -54,9 +57,11 @@ while true; do
     composer-cli --json compose info "${COMPOSE_ID}" | tee compose_info.json > /dev/null
     COMPOSE_STATUS=$(jq -r '.body.queue_status' compose_info.json)
 
+    echo "💤 Waiting for the compose to finish"
+
     # Is the compose finished?
     if [[ $COMPOSE_STATUS != RUNNING ]] && [[ $COMPOSE_STATUS != WAITING ]]; then
-        echo "Compose finished."
+        echo "🎉 Compose finished."
         break
     fi
     sleep 5
@@ -65,6 +70,6 @@ done
 if [[ $COMPOSE_STATUS != FINISHED ]]; then
     composer-cli compose logs ${COMPOSE_ID}
     podman-exec tar -axf /${COMPOSE_ID}-logs.tar logs/osbuild.log -O
-    echo "Something went wrong with the compose. 😢"
+    echo "😢 Something went wrong with the compose"
     exit 1
 fi
